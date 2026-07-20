54994106609e70b811187a3a487d736818e5703d
asifajasim08 <asifajasim08@gmail.com>
2026-07-20 10:58:32 +0530

    Add files via upload

---
+++ lenoflastword.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        // Remove spaces at the beginning and end
+        s = s.trim();
+        
+        // Split the string into words using one or more spaces
+        String[] words = s.split(" +");
+        
+        // Return the length of the last word
+        return words[words.length - 1].length();
+    }
+}
