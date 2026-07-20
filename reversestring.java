81457dbd019e7cd7ef866d4317aee3748a79fa2a
asifajasim08 <asifajasim08@gmail.com>
2026-07-20 10:50:48 +0530

    Add files via upload

---
+++ reversestring.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            // Swap characters
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp; // Fixed: Changed '5' to 's'
+
+            left++;
+            right--;
+        }
+    }
+}
